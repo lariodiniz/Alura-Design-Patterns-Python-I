d61edb134823c556a1dfd9b042d9589ad77ba684
LarioDiniz <lario.diniz@palo.com.br>
2018-06-28 12:39:23 -0300

    Aula 01
    Aula 01

---
+++ calculador_de_impostos.py
@@ -0,0 +1,24 @@
+
+from impostos import ICMS, ISS
+
+class Calculador_de_impostos(object):
+
+        def realiza_calculo(self, orcamento, imposto):    
+            imposto_calculado =imposto.calcula(orcamento)
+            print(imposto_calculado)
+
+
+if __name__ == '__main__':
+
+    print()
+    print('--CALCULADOR DE IMPOSTOS--')
+    from orcamento import Orcamento
+
+    calculador = Calculador_de_impostos()
+    orcamento = Orcamento(500)
+
+    calculador.realiza_calculo(orcamento, ISS())
+    calculador.realiza_calculo(orcamento, ICMS())
+
+
+    print('--         -*-          --')
